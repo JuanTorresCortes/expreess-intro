540f5ed2a3a3f2b48d47641e2dc141461918d701
Juan <juan.giana8589@gmail.com>
2023-05-16 14:50:56 -0500

    added movies.js array of movies

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const movies = require("./movies")
 const app = express();
 const PORT = 3000;
+
+app.get("/index", (req, res) => {
+    res.send("hello from express")
+});
+
+app.get("/about", (req, res) => {
+    res.status(200).send("hello from express")
+});
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
